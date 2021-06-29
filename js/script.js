7afd402f942b577a545a8ae01e4ee939b772410e
Luca-Riz <77754414+Luca-Riz@users.noreply.github.com>
2021-06-29 11:08:22 +0200

    add new contacts on js

--- js/script.js
+++ js/script.js
@@ -49,8 +49,99 @@ const app = new Vue(
                         }
                     ],
                 },
+
+                {
+                    name: 'Matteo',
+                    avatar: '_3',
+                    visible: true,
+                    messages: [
+                        {
+                        date: '20/03/2020 16:30:00',
+                        text: 'Ciao come stai?',
+                        status: 'sent'
+                        },
+                        {
+                        date: '20/03/2020 16:30:55',
+                        text: 'Bene grazie! Stasera ci vediamo?',
+                        status: 'received'
+                        },
+                        {
+                        date: '20/03/2020 16:35:00',
+                        text: 'Mi piacerebbe ma devo andare a fare la spesa.',
+                        status: 'sent'
+                        }
+                    ],
+                },
+
+                {
+                    name: 'Marco',
+                    avatar: '_4',
+                    visible: true,
+                    messages: [
+                        {
+                        date: '20/03/2020 16:30:00',
+                        text: 'Ciao come stai?',
+                        status: 'sent'
+                        },
+                        {
+                        date: '20/03/2020 16:30:55',
+                        text: 'Bene grazie! Stasera ci vediamo?',
+                        status: 'received'
+                        },
+                        {
+                        date: '20/03/2020 16:35:00',
+                        text: 'Mi piacerebbe ma devo andare a fare la spesa.',
+                        status: 'sent'
+                        }
+                    ],
+                },
+
+                {
+                    name: 'Filippo',
+                    avatar: '_5',
+                    visible: true,
+                    messages: [
+                        {
+                        date: '20/03/2020 16:30:00',
+                        text: 'Ciao come stai?',
+                        status: 'sent'
+                        },
+                        {
+                        date: '20/03/2020 16:30:55',
+                        text: 'Bene grazie! Stasera ci vediamo?',
+                        status: 'received'
+                        },
+                        {
+                        date: '20/03/2020 16:35:00',
+                        text: 'Mi piacerebbe ma devo andare a fare la spesa.',
+                        status: 'sent'
+                        }
+                    ],
+                },
+
+                {
+                    name: 'Lisa',
+                    avatar: '_6',
+                    visible: true,
+                    messages: [
+                        {
+                        date: '20/03/2020 16:30:00',
+                        text: 'Ciao come stai?',
+                        status: 'sent'
+                        },
+                        {
+                        date: '20/03/2020 16:30:55',
+                        text: 'Bene grazie! Stasera ci vediamo?',
+                        status: 'received'
+                        },
+                        {
+                        date: '20/03/2020 16:35:00',
+                        text: 'Mi piacerebbe ma devo andare a fare la spesa.',
+                        status: 'sent'
+                        }
+                    ],
+                },
             ],
-            iName: 0
         },
 
     }
